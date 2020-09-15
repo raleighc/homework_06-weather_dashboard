25847b72473979a571bc58bcaaa386e2025257a8
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-09-15 00:11:34 -0400

    added UV API and displayed it on page. Added concatination text to current city.

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,45 @@
 console.log("Hello Twitter World");
+// Checks to make sure entire document is loaded before jQuery fires.
 $(document).ready(function () {
 
+  var dateTime = moment().format("dddd, MMMM Do");
+
+
+
+  // onClick function that listens for City search submission.
   $("#searchBtn").on("click", function () {
     var cityName = $("#searchText").val();
-  // This is our API key. Add your own API key between the ""
+  // This is my API Key
   var APIKey = "f074459fc12a2702e389a5a7750c8cbb";
-  // Here we are building the URL we need to query the database
+  // URL we need to query the database
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
 
-  // We then created an AJAX call
+  // AJAX call to pull information from weather API
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    // Create CODE HERE to Log the queryURL
     console.log(queryURL);
-    // Create CODE HERE to log the resulting object
     console.log(response);
-    $("#currentCity").text(response.name);
-    $("#currentTemp").text(response.main.temp);
-    $("#currentHumid").text(response.main.humidity);
-    $("#currentWind").text(response.wind.speed);
-    
 
+    // Drilling for specific info located in the API
+    $("#currentCity").text(response.name + "  -  " + dateTime);
+    $("#currentTemp").text("Temperature:  " + Math.floor(response.main.temp) + " °F");
+    $("#currentHumid").text("Humidity:  " + response.main.humidity + "%");
+    $("#currentWind").text("Wind Speed:  " +response.wind.speed + " mph");
+
+    var lon = response.coord.lon;
+    var lat = response.coord.lat;
+    var queryURLuv = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+    // AJAX call for UV info only
+    $.ajax({
+      url: queryURLuv,
+      method: "GET",
+    }).then(function (response) {
+      var currentUVEl = response.value;
+    $("#currentUV").text("UV Index:  " + currentUVEl);
+  });
   });
 
   });
