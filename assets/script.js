a33ec5370e8c1de5d8b90edba19bb14dfef89f6e
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-09-15 01:11:30 -0400

    fixed the current city search info and got the UV working with conditionals.

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ console.log("Hello Twitter World");
 // Checks to make sure entire document is loaded before jQuery fires.
 $(document).ready(function () {
 
-  var dateTime = moment().format("dddd, MMMM Do");
+  var dateTime = (moment().format("MM/DD/YYYY"));
 
 
 
@@ -24,7 +24,7 @@ $(document).ready(function () {
     console.log(response);
 
     // Drilling for specific info located in the API
-    $("#currentCity").text(response.name + "  -  " + dateTime);
+    $("#currentCity").text(response.name + "  -  " + "(" + dateTime + ")");
     $("#currentTemp").text("Temperature:  " + Math.floor(response.main.temp) + " °F");
     $("#currentHumid").text("Humidity:  " + response.main.humidity + "%");
     $("#currentWind").text("Wind Speed:  " +response.wind.speed + " mph");
@@ -37,8 +37,21 @@ $(document).ready(function () {
       url: queryURLuv,
       method: "GET",
     }).then(function (response) {
-      var currentUVEl = response.value;
-    $("#currentUV").text("UV Index:  " + currentUVEl);
+      var UVIndex = response.value;
+      var spanUV = $("<span>").addClass("UVColored").text(UVIndex);
+      // console.log(currentUVEl);
+    $("#currentUV").text("UV Index:  ").append(spanUV);
+    if (UVIndex <= 2){
+      spanUV.attr("id", "low")
+    } else if (UVIndex >= 3 && UVIndex <= 5){
+      spanUV.attr("id", "med")
+    } else if (UVIndex >= 6 && UVIndex <= 7){
+      spanUV.attr("id", "high")
+    } else if (UVIndex >= 8 && UVIndex <= 10){
+      spanUV.attr("id", "veryHigh")
+    } else if (UVIndex >= 11){
+      spanUV.attr("id", "extremelyHigh")
+    }
   });
   });
 
